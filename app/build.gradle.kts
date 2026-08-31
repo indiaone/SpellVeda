c83c9878a76fcc32bd1848252d9d0773926f979f
indiaone <6.679267e+06+indiaone@users.noreply.github.com>
2026-08-31 01:33:07 +0000

    Prepare F-Droid-compatible 0.3.2 patch release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.srikanthg.spellveda"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
